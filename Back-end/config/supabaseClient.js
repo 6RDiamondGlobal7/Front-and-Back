ea99c25ddf080b5623341bdf4edb9114d2f2c447
Hansysy <hansrocillo21@gmail.com>
2026-04-05 01:37:58 +0800

    Update supabaseClient.js

--- Back-end/config/supabaseClient.js
+++ Back-end/config/supabaseClient.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Check if variables exist before creating the client
 if (!supabaseUrl || !supabaseAnonKey) {
